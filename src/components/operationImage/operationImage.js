2685cab78c6356515ae6294d3a4aabb20a756b16
theCrookedMan@163.com <theCrookedMan@163.com>
2019-05-05 18:09:49 +0800

    新增调试代码

--- src/components/operationImage/operationImage.js
+++ src/components/operationImage/operationImage.js
@@ -71,6 +71,7 @@ export default class operationImage {
       } else {
         img.src = imageInfo.operateStack[imageInfo.operateStackIndex]
       }
+      alert("img.src::::", img.src)
       
     })
   }
