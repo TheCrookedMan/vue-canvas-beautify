a4ba0f6148ec57c5f87f0bf6c7926db9410e78cd
theCrookedMan@163.com <theCrookedMan@163.com>
2019-06-19 15:20:31 +0800

    定位涂鸦页面自动刷新问题

--- src/components/operationImage/operationImage.js
+++ src/components/operationImage/operationImage.js
@@ -1045,6 +1045,7 @@ export default class operationImage {
     this.canvas.addEventListener('touchmove', this.doodleTouchMove, false)
   }
   drawLine() {
+    console.log("drawLine:::::")
     let _imageInfo = this.imageList[this.currentIndex];
     // this.clearCanvas()
     this.drawContext.beginPath()
